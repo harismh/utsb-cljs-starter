5632df079c1cb380c696146b48a9d64406fd57b8
Haris <hello@harism.dev>
2025-12-28 11:33:16 +0900

    Update starter deps - React 19.2.3 - UIx 1.4.8 - TailwindCSS 4.1.8 - ShadowCLJS 3.3.4 - Bun 1.3.0

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,10 +1,15 @@
 (ns app.main
   (:require [app.components :refer [app]]
+            [cljs.reader :as reader]
+            [shadow.resource :as resource]
             [uix.dom]
             [uix.core :refer [$]]))
+
+(def site-meta
+  (reader/read-string (resource/inline "meta.edn")))
 
 (defonce root
   (uix.dom/create-root (js/document.getElementById "root")))
 
 (defn ^:export ^:dev/after-load init []
-  (uix.dom/render-root ($ app) root))
+  (uix.dom/render-root ($ app site-meta) root))
